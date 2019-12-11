f712d24353683a66e85d3589a7e5d8695b2a12ee
bony-work <bf914@126.com>
2019-12-11 14:28:26 +0800

    修改路由中profile显示名称

--- src/router/index.js
+++ src/router/index.js
@@ -120,7 +120,7 @@ export const constantRoutes = [
         path: 'index',
         component: () => import('@/views/profile/index'),
         name: 'Profile',
-        meta: { title: '用户信息', icon: 'user', noCache: true }
+        meta: { title: '个人信息', icon: 'user', noCache: true }
       }
     ]
   }
